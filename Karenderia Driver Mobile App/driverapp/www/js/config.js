df531e0ef1f2a5b2cb5f5fa6d36e171a20610cbb
ozcan durak <info@ozcandurak.com>
2017-11-30 17:57:36 +0000

    Driver App

---
+++ Karenderia Driver Mobile App/driverapp/www/js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={	
+	'ApiUrl':"http://35.188.85.147/~leagrave/driver/api",
+	'DialogDefaultTitle':"MKRS",	
+	'PushProjectID':"",	
+	'APIHasKey':""
+};
